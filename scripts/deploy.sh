9b41df844e68daa29cceb1834f3670d7985c1d58
Shuai Lin <linshuai2012@gmail.com>
2018-04-24 16:37:05 +0800

    Improved deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,14 +12,16 @@ if [[ $TRAVIS_BRANCH != "master" ]]; then
 fi
 
 mkdir -p ~/.ssh
+cat >~/.ssh/config <<EOF
+Host *
+     UserKnownHostsFile /dev/null
+     StrictHostKeyChecking no
+EOF
+
 openssl aes-256-cbc -K $encrypted_0138e8c4859f_key -iv $encrypted_0138e8c4859f_iv -in scripts/key.txt.enc -out ~/.ssh/id_rsa -d
 chmod 400 ~/.ssh/id_rsa
 
-do_ssh() {
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$@"
-}
-
-if ! do_ssh $DEPLOY_USER@$DEPLOY_SERVER $DEPLOY_CMD seafile-docs; then
+if ! ssh $DEPLOY_USER@$DEPLOY_SERVER $DEPLOY_CMD seafile-docs; then
     echo "Failed to deploy"
     exit 1
 fi
